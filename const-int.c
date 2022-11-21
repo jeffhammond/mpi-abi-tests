ba68f29ddf94532028d78e1f312ace5ff59ecf60
Jeff Hammond <jeff.science@gmail.com>
2022-11-21 11:47:59 +0200

    more

--- const-int.c
+++ const-int.c
@@ -1,6 +1,15 @@
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
 {
     const int x = 2;
-    x = 3;
+    const int y = 3;
+    int z = (argc > 1) ? atoi(argv[1]) : 2;
+    switch (z) {
+        case x: return x;
+        case y: return y;
+        default: return -1;
+    }
     return x;
 }
