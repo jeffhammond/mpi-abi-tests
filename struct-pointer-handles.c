615273864afdbb5424563f6e340f0b02a58d815b
Jeff Hammond <jeff.science@gmail.com>
2022-11-21 14:46:49 +0200

    allocate array of statuses

--- struct-pointer-handles.c
+++ struct-pointer-handles.c
@@ -5,15 +5,12 @@
 typedef struct ABI_Comm * MPI_Comm;
 typedef struct ABI_Group * MPI_Group;
 typedef struct ABI_Datatype * MPI_Datatype;
+typedef struct ABI_Status * MPI_Status;
+typedef struct ABI_Request * MPI_Request;
 
 MPI_Comm MPI_COMM_WORLD;
 MPI_Group MPI_GROUP_WORLD;
 MPI_Datatype MPI_BYTE;
-
-typedef struct {
-    intptr_t val;
-} MPI_Request;
-
 
 int MPI_Ibcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm, MPI_Request *request)
 {
@@ -32,5 +29,9 @@ int main(void)
     rc = MPI_Ibcast(&x, sizeof(x), MPI_BYTE, 0, MPI_COMM_WORLD, &r);
     rc = MPI_Ibcast(&x, sizeof(x), 0, MPI_BYTE, MPI_COMM_WORLD, &r);
     rc = MPI_Ibcast(&x, sizeof(x), MPI_BYTE, 0, MPI_GROUP_WORLD, &r);
+
+    MPI_Status * as = malloc( 100 * sizeof(MPI_Status) );
+    free(as);
+
     return rc;
 }
