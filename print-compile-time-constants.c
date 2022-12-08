134068e46835ff8142fe6cecd9ab2ab1598f39fb
Jeff Hammond <jeff.science@gmail.com>
2022-12-08 13:27:13 +0200

    fix for MPI-3

--- print-compile-time-constants.c
+++ print-compile-time-constants.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    printf("MPI_VERSION=%d\n",MPI_VERSION);
+    printf("MPI_SUBVERSION=%d\n",MPI_SUBVERSION);
     printf("MPI_MAX_PROCESSOR_NAME=%d\n",MPI_MAX_PROCESSOR_NAME);
     printf("MPI_MAX_LIBRARY_VERSION_STRING=%d\n",MPI_MAX_LIBRARY_VERSION_STRING);
     printf("MPI_MAX_ERROR_STRING=%d\n",MPI_MAX_ERROR_STRING);
@@ -11,5 +13,11 @@ int main(void)
     printf("MPI_MAX_INFO_VAL=%d\n",MPI_MAX_INFO_VAL);
     printf("MPI_MAX_OBJECT_NAME=%d\n",MPI_MAX_OBJECT_NAME);
     printf("MPI_MAX_PORT_NAME=%d\n",MPI_MAX_PORT_NAME);
+#ifdef MPI_MAX_STRINGTAG_LEN
+    printf("MPI_MAX_STRINGTAG_LEN=%d\n",MPI_MAX_STRINGTAG_LEN);
+#endif
+#ifdef MPI_MAX_PSET_NAME_LEN
+    printf("MPI_MAX_PSET_NAME_LEN=%d\n",MPI_MAX_PSET_NAME_LEN);
+#endif
     return 0;
 }
