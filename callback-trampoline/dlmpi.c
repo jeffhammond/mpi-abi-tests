a9d7b1dd1bd3649bb7f84bbafb3b9c3c45ff9b26
Jeff Hammond <jeff.science@gmail.com>
2023-01-29 10:51:16 +0200

    hello world works

--- callback-trampoline/dlmpi.c
+++ callback-trampoline/dlmpi.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <dlfcn.h>
 
@@ -11,8 +12,33 @@
 #error Unsupported OS
 #endif
 
+typedef enum {
+    MPICH   = 1,
+    OMPI    = 2,
+    UNKNOWN = 3
+} Which_MPI_e;
+
+// ABI-independent symbols
+int (*MPI_Init)(int*,char***);
+int (*MPI_Finalize)(void);
+int (*MPI_Get_library_version)(char*,int*);
+
+// ABI-dependent symbols
+int (*OMPI_Comm_rank)(void*,int*);
+int (*OMPI_Comm_size)(void*,int*);
+typedef void * OMPI_Comm;
+const char * OMPI_COMM_WORLD_symbol = "ompi_mpi_comm_world";
+OMPI_Comm OMPI_COMM_WORLD;
+
+int (*MPICH_Comm_rank)(int,int*);
+int (*MPICH_Comm_size)(int,int*);
+typedef int MPICH_Comm;
+MPICH_Comm MPICH_COMM_WORLD = 0x44000000;
+
 int main(int argc, char* argv[])
 {
+    int rc;
+
     char * libmpi_name;
     if (argc > 1) {
         libmpi_name = argv[1];
@@ -20,15 +46,72 @@ int main(int argc, char* argv[])
         libmpi_name = LIBMPI_NAME;
     }
 
-    printf("attempting to dlopen %s\n", libmpi_name);
+    //printf("attempting to dlopen %s\n", libmpi_name);
     void * MPI = dlopen(libmpi_name, RTLD_LAZY);
     if (MPI == NULL) {
         printf("dlopen failed: %s\n", dlerror() );
         abort();
     } else {
-        printf("dlopen succeeded: %p\n", MPI);
+        //printf("dlopen succeeded: %p\n", MPI);
     }
 
+    MPI_Init = (int (*)(int*,char***))dlsym(MPI, "MPI_Init");
+    MPI_Finalize = (int (*)(void))dlsym(MPI, "MPI_Finalize");
+    MPI_Get_library_version = (int (*)(char*,int*))dlsym(MPI, "MPI_Get_library_version");
 
-    return 0;
+    rc = MPI_Init(&argc,&argv);
+
+    // figure out which library i am using
+    Which_MPI_e whose_mpi;
+    {
+        char lib_version[16384] = {0};
+        int  lib_version_length;
+        rc = MPI_Get_library_version(lib_version, &lib_version_length);
+        //printf("MPI_Get_library_version = %s\n", lib_version);
+
+        char * pos;
+        pos = strstr(lib_version, "MPICH");
+        if (pos != NULL) {
+            printf("The MPI is MPICH\n");
+            whose_mpi = MPICH;
+        }
+        pos = strstr(lib_version, "Open MPI");
+        if (pos != NULL) {
+            printf("The MPI is OMPI\n");
+            whose_mpi = OMPI;
+        }
+    }
+
+    // interact with MPI_Comm
+    {
+        int me, np;
+
+        if (whose_mpi == OMPI)
+        {
+            OMPI_COMM_WORLD = dlsym(MPI, OMPI_COMM_WORLD_symbol);
+            if (OMPI_COMM_WORLD == NULL) {
+                printf("dlsym failed: %s\n", dlerror() );
+            } else {
+                printf("dlsym succeeded: %p\n", OMPI_COMM_WORLD);
+            }
+            OMPI_Comm_rank = (int(*)(void*,int*))dlsym(MPI, "MPI_Comm_rank");
+            rc = OMPI_Comm_rank(OMPI_COMM_WORLD, &me);
+            OMPI_Comm_size = (int(*)(void*,int*))dlsym(MPI, "MPI_Comm_size");
+            rc = OMPI_Comm_size(OMPI_COMM_WORLD, &np);
+            printf("I am %d of %d\n", me, np);
+        }
+        else if (whose_mpi == MPICH)
+        {
+            MPICH_Comm_rank = (int(*)(int,int*))dlsym(MPI, "MPI_Comm_rank");
+            rc = MPICH_Comm_rank(MPICH_COMM_WORLD, &me);
+            MPICH_Comm_size = (int(*)(int,int*))dlsym(MPI, "MPI_Comm_size");
+            rc = MPICH_Comm_size(MPICH_COMM_WORLD, &np);
+            printf("I am %d of %d\n", me, np);
+        }
+
+    }
+
+    rc = MPI_Finalize();
+
+    return rc;
 }
